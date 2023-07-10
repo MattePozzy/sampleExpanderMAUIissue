a9944d44330c9e6e725720485dc2c3f32ed68db4
Matteo <7420345+Raystorm7@users.noreply.github.com>
2023-07-10 10:03:40 +0200

    Add files via upload

---
+++ Project2/MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Maui;
+using MauiReactor;
+using Project2.Pages;
+
+
+namespace Project2
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiReactorApp<MainPage>(app =>
+                {
+                    app.AddResource("Resources/Styles/Colors.xaml");
+                    app.AddResource("Resources/Styles/Styles.xaml");
+
+                    app.SetWindowsSpecificAssetsDirectory("Assets");
+                })
+#if DEBUG
+            .EnableMauiReactorHotReload()
+#endif
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
+                });
+            builder.UseMauiCommunityToolkit();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Project2/Pages/MainPage.cs
@@ -0,0 +1,68 @@
+﻿using MauiReactor;
+
+namespace Project2.Pages
+{
+    [Scaffold(typeof(CommunityToolkit.Maui.Views.Expander))]
+    public partial class VolosExpander { }
+
+    internal class MainPageState
+    {
+        public int Counter { get; set; }
+    }
+
+    internal class MainPage : Component<MainPageState>
+    {
+        public override VisualNode Render()
+        {
+            Button btnHeader = new Button()
+                .Text("H")
+                //.ImageSource(new MauiControls.FontImageSource()
+                //{
+                //    FontAutoScalingEnabled = true,
+                //    FontFamily = Costanti.Font.MaterialDesignIcons.ToString(),
+                //    Glyph = MaterialFontIcons.Plus
+                //})
+                .BackgroundColor(Colors.Green).CornerRadius(80)
+                .HorizontalOptions(MauiControls.LayoutOptions.End)
+                .VerticalOptions(MauiControls.LayoutOptions.End);
+
+            Button btnContent1 = new Button()
+                .Text("C1")
+                //.ImageSource(new MauiControls.FontImageSource()
+                //{
+                //    FontAutoScalingEnabled = true,
+                //    FontFamily = Costanti.Font.MaterialDesignIcons.ToString(),
+                //    Glyph = MaterialFontIcons.Plus
+                //})
+                .BackgroundColor(Colors.Red).CornerRadius(80)
+                .HorizontalOptions(MauiControls.LayoutOptions.End)
+                .VerticalOptions(MauiControls.LayoutOptions.End);
+
+            Button btnContent2 = new Button()
+                .Text("C2")
+                //.ImageSource(new MauiControls.FontImageSource()
+                //{
+                //    FontAutoScalingEnabled = true,
+                //    FontFamily = Costanti.Font.MaterialDesignIcons.ToString(),
+                //    Glyph = MaterialFontIcons.Plus
+                //})
+                .BackgroundColor(Colors.Blue).CornerRadius(80)
+                .HorizontalOptions(MauiControls.LayoutOptions.End)
+                .VerticalOptions(MauiControls.LayoutOptions.End);
+
+            return new ContentPage
+            {
+                new VolosExpander()
+                {
+                    new VStack()
+                    {
+                        btnContent1,
+                        btnContent2,
+                    }.Spacing(20)
+                }
+                .Header(TemplateHost.Create(btnHeader).NativeElement as MauiControls.Button)
+                .VCenter().HCenter()
+            };
+        }
+    }
+}
